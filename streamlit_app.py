71f0a205ada6e1ad5ca18eab0dc2697ca048c699
almyser1 <amanda.myser@becu.org>
2025-05-09 08:54:36 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,8 +20,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 
 #Convert the Snowpark Dataframe to a Pandas Dataframe so we can use the LOC function
 pd_df=my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+#st.dataframe(pd_df)
+#st.stop()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
@@ -33,20 +33,23 @@ if ingredients_list:
     ingredients_string = ''
 
     for fruit_chosen in ingredients_list:
-      ingredients_string += fruit_chosen + ' '
-      st.subheader(fruit_chose + ' Nutrition Information')
-      smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" = fruit_chosen)
-      st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
-      
-    #st.write(ingredients_string)
+        ingredients_string += fruit_chosen + ' '
 
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order )
+        search_on = pd_df.loc[pd_df]['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for', fruit_chosen, ' is ', search_on, '.')      
+        st.subheader(fruit_chose + ' Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" = fruit_chosen)
+        st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+      
+        #st.write(ingredients_string)
+
+        my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order )
             values ('""" + ingredients_string + """','""" + name_on_order + """')"""
 
-    #st.write(my_insert_stmt)
-    #st.stop()
+        #st.write(my_insert_stmt)
+        #st.stop()
     
-    time_to_insert = st.button("Submit Order")
+        time_to_insert = st.button("Submit Order")
 
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
